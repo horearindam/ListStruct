f8476af52adf8a46c0de4aee065b5063d5d90911
horearindam <connect.arindamhore@gmail.com>
2026-06-16 19:18:50 +0530

    Handle Errors

--- main.c
+++ main.c
@@ -1,24 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
+#include <errno.h>
 
-int main(int argc, char *argv[]) {
-    const char *path = (argc > 1) ? argv[1] : ".";
+int main(int argc, char *argv[])
+{
+    if (argc > 2) {
+        fprintf(stderr, "Usage: %s [directory]\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    const char *path = (argc == 2) ? argv[1] : ".";
 
     DIR *dir = opendir(path);
-    if(!dir) {
-        perror("Error");
-        return 1;
+    if (dir == NULL) {
+        perror(path);
+        return EXIT_FAILURE;
     }
+
+    errno = 0;
 
     struct dirent *entry;
-    while((entry = readdir(dir)) != NULL) {
-        if(entry->d_name[0] == '.') {
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_name[0] == '.')
             continue;
+
+        if (printf("%s\n", entry->d_name) < 0) {
+            perror("printf");
+            closedir(dir);
+            return EXIT_FAILURE;
         }
-        printf("%s\n", entry->d_name);
     }
 
-    closedir(dir);
+    if (errno != 0) {
+        perror("readdir");
+        closedir(dir);
+        return EXIT_FAILURE;
+    }
 
-    return 0;
+    if (closedir(dir) == -1) {
+        perror("closedir");
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }

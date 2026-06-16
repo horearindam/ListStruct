730732324b411539d9abf2fa5cf6dfe2b6a36f0f
horearindam <connect.arindamhore@gmail.com>
2026-06-16 19:12:10 +0530

    Simple Implementation to ls

--- main.c
+++ main.c
@@ -10,5 +10,13 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    struct dirent *entry;
+    while((entry = readdir(dir)) != NULL) {
+        if(entry->d_name[0] == '.') {
+            continue;
+        }
+        printf("%s\n", entry->d_name);
+    }
+
     return 0;
 }

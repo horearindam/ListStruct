dc6f3a7884789950a1e9e79925e3f1faf6c8f423
horearindam <connect.arindamhore@gmail.com>
2026-06-16 18:51:42 +0530

    Try perror()

--- main.c
+++ main.c
@@ -5,5 +5,10 @@ int main(int argc, char *argv[]) {
     const char *path = (argc > 1) ? argv[1] : ".";
 
     DIR *dir = opendir(path);
+    if(!dir) {
+        perror("Error");
+        return 1;
+    }
+
     return 0;
 }

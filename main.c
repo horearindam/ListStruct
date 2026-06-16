684b110bede412aad3f78c6ba4203fa993cf427d
horearindam <connect.arindamhore@gmail.com>
2026-06-16 18:44:55 +0530

    Figure out the opendir()

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <dirent.h>
 
 int main(int argc, char *argv[]) {
     const char *path = (argc > 1) ? argv[1] : ".";
 
-    printf("%s\n", path);
+    DIR *dir = opendir(path);
     return 0;
 }
